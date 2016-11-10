42fc88eeb70733f2265fa1032e855e0e4540a8df
Markiyan Vaskiv <vaskiv.markiyan@gmail.com>
2016-11-10 13:13:04 +0000

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,20 +1,34 @@
-var numbFib = Number(prompt('Enter number [1; +∞)'));
+var numbFib = Number(prompt('Enter natural number [1; +∞)'));
 while (numbFib <= 0) {
-    numbFib = Number(prompt('Enter correct number! From 1 to +∞'));
+    numbFib = Number(prompt('Enter correct natural number! From 1 to +∞'));
 }
-function fibonacci(numbFib) {
+var resultLoop;
+var resultRecursion;
+function fibonacciLoop(numbFib) {
     var arrFib = [1, 1];
     if (numbFib === 1) {
-        document.write('Fibonacci sequence: ' + arrFib[0] + '<br>');
-        document.write('Your ' + numbFib + ' Fibonacci number: ' + arrFib[0] + '<br>');
+        document.write('Fibonacci sequence: ' + arrFib[0]);
+        return resultLoop = arrFib[0];
     } else if (numbFib >= 2) {
-        document.write('Fibonacci sequence: ' + arrFib[0] + arrFib[1]);
+        document.write('Fibonacci sequence: ' + arrFib[0] + ' ' + arrFib[1] + ' ');
         for (var i = 2; i < numbFib; i++) {
             arrFib[i] = arrFib[i - 2] + arrFib[i - 1];
             arrFib.push(arrFib[i]);
-            document.write(arrFib[i]);
+            document.write(arrFib[i] + ' ');
         }
-        document.write('<br> Your ' + numbFib + ' Fibonacci number: ' + arrFib[numbFib - 1]);
+        return resultLoop = arrFib[numbFib - 1];
     }
 }
-fibonacci (numbFib);
+document.write('<b>By loop:</b> <br>')
+fibonacciLoop(numbFib);
+document.write('<br> Fibonacci number ' + numbFib + ': ' + '<u>' + resultLoop + '</u>' + '<br> <br>');
+function fibonacciRecursion(numbFib) {
+    if (numbFib <= 1) {
+        return resultRecursion = numbFib;
+    } else if (numbFib > 1) {
+        return resultRecursion = fibonacciRecursion(numbFib - 2) + fibonacciRecursion(numbFib - 1);
+    }
+}
+document.write('<b>By recursion:</b> <br>');
+fibonacciRecursion(numbFib);
+document.write('Fibonacci number ' + numbFib + ': ' + '<u>' + resultRecursion + '</u>');
